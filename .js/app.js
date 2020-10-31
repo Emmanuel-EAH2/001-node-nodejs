f566924d8fd44fc0da3949345e38609045cfb3e5
Emmanuel-EAH2 <cetis634@gmail.com>
2020-10-30 23:03:17 -0600

    se guardo tomo bien bro

---
+++ .js/app.js
@@ -0,0 +1,9 @@
+// Pregunta = prompt("Cual es tu nombre?");
+// nombre='';
+
+
+// if(nombre = ''){
+//     alert().remove();
+// }else{
+//     alert(`¡${Pregunta} ${nombre}hola la ESFC te regala un fuerte abrazo y esperando que la gracia de Dios este contigo y con tu familia que disfrutes tu estadia en nuestra pagina atte: ESFCoficial!`);
+// }
